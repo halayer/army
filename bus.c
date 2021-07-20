03ae32444f83e3b648fa603c2542af0792560a2e
H. A. L <31424568+halayer@users.noreply.github.com>
2021-07-20 23:17:52 +0200

    Update bus.c

--- bus.c
+++ bus.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "bus.h"
 
 Bus *Bus_new() {
@@ -9,22 +10,76 @@ Bus *Bus_new() {
 	return bus;
 }
 
-int Bus_registerComponent(Bus *bus, void *component) {
-	memcpy(component, &bus, sizeof(Bus *));
-	
-	if (bus->total_components == 16)
+int Bus_registerComponent(Bus *bus, Component *component) {
+	if (bus->component_count == 16)
 		return -1;
 	
-	bus->components[bus->total_components] = component;
-	bus->total_components++;
+	bus->components[bus->component_count] = *component;
+	bus->component_count++;
 	
 	return 0;
 }
 
-WORD Bus_read(Bus *bus, WORD address, int *error) {
-	return ((WORD **)(bus->components[1] + sizeof(Bus *)))[0][address>>2];
+int Bus_findComponentByAddr(Bus *bus, WORD addr) {
+	Component *comp;
+	
+	for (int i = 0; i < bus->component_count; i++) {
+		comp = &bus->components[i];
+		if (!comp->has_addr_space) continue;
+		if (!comp->is_in_addr_space(comp->inst, addr)) continue;
+		return i;
+	}
+	
+	return -1;
 }
 
-int Bus_write(Bus *bus, WORD address, WORD data, int mask, int *error) {
-	((WORD **)(bus->components[1] + sizeof(Bus *)))[0][address>>2] = (Bus_read(bus, address, error) & ~mask) | data;
+uint8_t Bus_read8(Bus *bus, WORD addr, int *error) {
+	return Bus_read32(bus, addr, error) & 0xFF;
+}
+
+uint16_t Bus_read16(Bus *bus, WORD addr, int *error) {
+	return Bus_read32(bus, addr, error) & 0xFFFF;
+}
+
+uint32_t Bus_read32(Bus *bus, WORD addr, int *error) {
+	int comp_index = Bus_findComponentByAddr(bus, addr);
+	if (comp_index < 0) {
+		*error = 1;
+		return 0;
+	}
+	
+	Component *comp = &bus->components[comp_index];
+	return comp->read_word(comp->inst, addr);
+}
+
+int Bus_write8(Bus *bus, WORD addr, uint8_t data) {
+	return Bus_write32(bus, addr, (Bus_read32(bus, addr, NULL) & 0xFFFFFF00) | data);
+}
+
+int Bus_write16(Bus *bus, WORD addr, uint16_t data) {
+	return Bus_write32(bus, addr, (Bus_read32(bus, addr, NULL) & 0xFFFF0000) | data);
+}
+
+int Bus_write32(Bus *bus, WORD addr, uint32_t data) {
+	int comp_index = Bus_findComponentByAddr(bus, addr);
+	if (comp_index < 0) return -1;
+	
+	Component *comp = &bus->components[comp_index];
+	return comp->write_word(comp->inst, addr, data);
+}
+
+int Bus_readArea(Bus *bus, WORD addr, int size, void *dst) {
+	int comp_index = Bus_findComponentByAddr(bus, addr);
+	if (comp_index < 0) return -1;
+	
+	Component *comp = &bus->components[comp_index];
+	return comp->read_area(comp->inst, addr, size, dst);
+}
+
+int Bus_writeArea(Bus *bus, WORD addr, int size, void *src) {
+	int comp_index = Bus_findComponentByAddr(bus, addr);
+	if (comp_index < 0) return -1;
+	
+	Component *comp = &bus->components[comp_index];
+	return comp->write_area(comp->inst, addr, size, src);
 }
