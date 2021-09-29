72b166d38dd622e6927410c5b9cadb79afcd4d5c
H. A. L <31424568+halayer@users.noreply.github.com>
2021-09-29 17:11:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 #include "isa.h"
 
 typedef struct RAM {
-	Bus *bus;
-	WORD *mem;
+    Bus *bus;
+    WORD *mem;
 } RAM;
 
 int RAM_checkAddrSpace(void *inst, WORD addr) { return 1; }
@@ -17,33 +17,33 @@ int RAM_readArea(void *inst, WORD addr, int size, void *dst) { memcpy(dst, (void
 int RAM_writeArea(void *inst, WORD addr, int size, void *src) { memcpy((void *)((RAM *)inst)->mem, src, size*4); }
 
 int main() {
-	Bus *bus = Bus_new();
-	ARM *cpu = ARM_new(ARCH_ARM9);
-	RAM ram; ram.bus = bus;
-	Component ram_comp = {1, RAM_checkAddrSpace, RAM_readWord, RAM_writeWord, RAM_readArea, RAM_writeArea};
-	ram_comp.inst = &ram;
-	
-	cpu->debug = stdout;
-	cpu->bus = bus;
-	
-	ram.mem = malloc(512);
-	memset(ram.mem, 0, 512);
-	
-	// LDR r0, [r15]
-	// MOV r0, #0x80000000
-	// MOV r0, r1, LSR r2
-	// MOV r0, r1
-	RAM_writeArea((void *)&ram, 0, 4, (void *)((WORD *)"\x00\x10\x9f\xe5\x02\x01\xa0\xe3\x31\x02\xa0\xe1\x01\x00\xa0\xe1"));
-	
-	Bus_registerComponent(bus, &ram_comp);
-	
-	ARM_reset(cpu);
-	
-	do {
-		ARM_step(cpu);
-	} while (getchar() != 104); // "H"
-	
-	ARM_registerDump(cpu);
-	
-	return 0;
+    Bus *bus = Bus_new();
+    ARM *cpu = ARM_new(ARCH_ARM9);
+    RAM ram; ram.bus = bus;
+    Component ram_comp = {1, RAM_checkAddrSpace, RAM_readWord, RAM_writeWord, RAM_readArea, RAM_writeArea};
+    ram_comp.inst = &ram;
+
+    cpu->debug = stdout;
+    cpu->bus = bus;
+
+    ram.mem = malloc(512);
+    memset(ram.mem, 0, 512);
+
+    // LDR r0, [r2], r1, ASR #5
+    // MOV r0, #0x80000000
+    // MOV r0, r1, LSR r2
+    // MOV r0, r1
+    RAM_writeArea((void *)&ram, 0, 4, (void *)((WORD *)"\xc1\x02\x92\xe6\x02\x01\xa0\xe3\x31\x02\xa0\xe1\x01\x00\xa0\xe1"));
+
+    Bus_registerComponent(bus, &ram_comp);
+
+    ARM_reset(cpu);
+
+    do {
+        ARM_step(cpu);
+    } while (getchar() != 104); // "H"
+
+    ARM_registerDump(cpu);
+
+    return 0;
 }
