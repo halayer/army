b0f05d4953e76a32eae20794382463504432181c
H. A. L <31424568+halayer@users.noreply.github.com>
2022-01-23 21:35:13 +0100

    Update instr.h

--- include/instr.h
+++ include/instr.h
@@ -21,7 +21,9 @@ int ARMISA_MOV(ARM *cpu, ARMISA_InstrInfo *info);
 int ARMISA_BIC(ARM *cpu, ARMISA_InstrInfo *info);
 int ARMISA_MVN(ARM *cpu, ARMISA_InstrInfo *info);
 int ARMISA_B(ARM *cpu, ARMISA_InstrInfo *info);
+int ARMISA_BX(ARM *cpu, ARMISA_InstrInfo *info);
 int ARMISA_BL(ARM *cpu, ARMISA_InstrInfo *info);
+int ARMISA_BLX(ARM *cpu, ARMISA_InstrInfo *info);
 int ARMISA_MUL(ARM *cpu, ARMISA_InstrInfo *info);
 int ARMISA_MLA(ARM *cpu, ARMISA_InstrInfo *info);
 int ARMISA_UMULL(ARM *cpu, ARMISA_InstrInfo *info);
@@ -34,23 +36,23 @@ int ARMISA_STR(ARM *cpu, ARMISA_InstrInfo *info);
 
 enum ARMISA_mnemonic {
     AND, EOR, SUB, RSB, ADD, ADC, SBC, RSC,	TST, TEQ,
-    CMP, CMN, ORR, MOV, BIC, MVN, B,   BL,  MUL, MLA,
-    UMULL, SMULL, UMLAL, SMLAL, LDR, STR,
+    CMP, CMN, ORR, MOV, BIC, MVN, B,   BX,  BL,  BLX,
+    MUL, MLA, UMULL, SMULL, UMLAL, SMLAL, LDR, STR,
 };
 
 cycleFunc instr_func_lookup[] = {
     ARMISA_AND, ARMISA_EOR, ARMISA_SUB, ARMISA_RSB, ARMISA_ADD,
     ARMISA_ADC, ARMISA_SBC, ARMISA_RSC, ARMISA_TST, ARMISA_TEQ,
     ARMISA_CMP, ARMISA_CMN, ARMISA_ORR, ARMISA_MOV, ARMISA_BIC,
-    ARMISA_MVN, ARMISA_B,	ARMISA_BL,	ARMISA_MUL,	ARMISA_MLA,
-    ARMISA_UMULL, ARMISA_SMULL, ARMISA_UMLAL, ARMISA_SMLAL,
-    ARMISA_LDR, ARMISA_STR,
+    ARMISA_MVN, ARMISA_B,	ARMISA_BX,  ARMISA_BL,  ARMISA_BLX,
+    ARMISA_MUL,	ARMISA_MLA,  ARMISA_UMULL, ARMISA_SMULL,
+    ARMISA_UMLAL, ARMISA_SMLAL, ARMISA_LDR, ARMISA_STR,
 };
 
 char *mnemonic_lookup[] = {
     "AND", "EOR", "SUB", "RSB", "ADD", "ADC", "SBC", "RSC", "TST", "TEQ",
-    "CMP", "CMN", "ORR", "MOV", "BIC", "MVN", "B",	 "BL",	"MUL", "MLA",
-    "UMULL", "SMULL", "UMLAL", "SMLAL","LDR", "STR",
+    "CMP", "CMN", "ORR", "MOV", "BIC", "MVN", "B",	 "BX",  "BL",  "BLX",
+    "MUL", "MLA", "UMULL", "SMULL", "UMLAL", "SMLAL","LDR", "STR",
 };
 
 #endif
